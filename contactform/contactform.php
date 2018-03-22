141011ea1b8347504b91bd2702e0a226050cfdd5
Gustavo Pinho <gustavo.pinho@gavb.com.br>
2018-03-22 14:12:50 -0300

    email contact

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -4,7 +4,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
-$to = 'gcdpinho@gmail.com';
+$to = 'valeeuropeu@superforte.net.br';
 $email_subject = "Mensagem do site:  $name";
 $email_body = "Você recebeu uma nova mensagem do seu site.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nE-mail: $email\n\Assunto: $subject\n\nMensagem:\n$message";
 $headers = "De: noreply@vulpis.com.br\n";
