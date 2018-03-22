de01a9e8c3aae6cd9a635dd794ac8876b886874e
gcdpinho <gcdpinho@gmail.com>
2018-03-21 22:39:20 -0300

    fix contact

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,12 +1,12 @@
 <?php
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
-$companyName = strip_tags(htmlspecialchars($_POST['companyName']));
+$subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 $to = 'gcdpinho@gmail.com';
 $email_subject = "Mensagem do site:  $name";
-$email_body = "Você recebeu uma nova mensagem do seu site.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nE-mail: $email\n\nEmpresa: $companyName\n\nMensagem:\n$message";
+$email_body = "Você recebeu uma nova mensagem do seu site.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nE-mail: $email\n\Assunto: $subject\n\nMensagem:\n$message";
 $headers = "De: noreply@vulpis.com.br\n";
 $headers .= "Responder a: $email";	
 mail($to,$email,$email_body,$headers);
